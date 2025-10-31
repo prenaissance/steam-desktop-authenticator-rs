a581322574267d202e1f54f992664de3b1f5b2af
Marcel Prodan <nuta228322@gmail.com>
2025-10-31 16:05:16 +0200

    Reverted BE changes

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -18,9 +18,7 @@ pub struct AppState {
 
 impl AppState {
     fn init(app: &App) -> Result<Self, Box<dyn Error>> {
-        let config_dir = app.path().app_config_dir()?;
-        let config_path = config_dir.join("config.json");
-
+        let config_path = app.path().app_config_dir()?.join("config.json");
         Ok(AppState {
             accounts_config: AccountsConfig::from_config(&config_path)
                 .inspect_err(|err| match err {
@@ -39,8 +37,6 @@ impl AppState {
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
-        .plugin(tauri_plugin_fs::init())
-        .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_opener::init())
         .setup(|app| {
             app.manage(Mutex::new(AppState::init(&app)?));
@@ -52,10 +48,7 @@ pub fn run() {
             approval::get_otp,
             account_manager::is_logged_in,
             account::commands::get_profile,
-            auth::commands::get_stored_accounts,
-            auth::commands::store_account,
-            auth::commands::remove_account,
-            auth::commands::set_active_account
+            account_manager::is_logged_in
         ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
