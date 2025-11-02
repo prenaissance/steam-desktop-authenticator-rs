e78b392d58f2bb8e0865ead844022e46fbfe1455
Marcel Prodan <nuta228322@gmail.com>
2025-11-02 15:08:54 +0200

    feat(FE+BE): Dynamic resize of the window

--- src/app.tsx
+++ src/app.tsx
@@ -1,4 +1,5 @@
 import "./index.css";
+import { PhysicalSize, Window } from "@tauri-apps/api/window";
 import { BrowserRouter, Route, Routes } from "react-router";
 import { Toaster } from "./components/ui/sonner";
 import { Layout } from "./layout";
@@ -7,6 +8,20 @@ import { TotpPage } from "./pages/app/totp";
 import { AuthSteamPage } from "./pages/auth/steam";
 import { WelcomePage } from "./pages/welcome";
 import { AccountsProvider } from "./providers/accounts-provider";
+
+async function resizeToContent() {
+  requestAnimationFrame(async () => {
+    const content = document.documentElement;
+    const width = content.scrollWidth;
+    const height = content.scrollHeight;
+
+    const window = new Window("main");
+
+    await window.setSize(new PhysicalSize(width, height));
+  });
+}
+
+window.addEventListener("DOMContentLoaded", resizeToContent);
 
 export const App = () => (
   <>
