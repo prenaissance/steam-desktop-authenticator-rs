afb6b9349e162b71a1d721f7d6f879097115c48c
prenaissance <95138356+prenaissance@users.noreply.github.com>
2025-11-08 18:03:33 +0200

    chore: format files

--- src/api/authentication-approvals.ts
+++ src/api/authentication-approvals.ts
@@ -75,7 +75,7 @@ export enum AuthApprovalError {
 }
 
 export const approveSession = async (
-  payload: AuthApproveRequest
+  payload: AuthApproveRequest,
 ): Promise<void> =>
   invoke<void>("approve_session", {
     payload: {
@@ -88,7 +88,7 @@ export const useApproveSession = (
   options?: Omit<
     UseMutationOptions<void, AuthApprovalError, AuthApproveRequest>,
     "mutationFn"
-  >
+  >,
 ) => {
   const queryClient = useQueryClient();
   const { onSuccess, ...rest } = options || {};
@@ -114,7 +114,7 @@ export const useDenySession = (
   options?: Omit<
     UseMutationOptions<void, AuthApprovalError, AuthDenyRequest>,
     "mutationFn"
-  >
+  >,
 ) => {
   const queryClient = useQueryClient();
   const { onSuccess, ...rest } = options || {};
