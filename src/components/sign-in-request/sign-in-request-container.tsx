db2865cef8e0c17e8d7d2b5c2ceb7201db094efb
prenaissance <95138356+prenaissance@users.noreply.github.com>
2025-11-11 21:38:47 +0200

    feat(frontend): redesign sign in request list

--- src/components/sign-in-request/sign-in-request-container.tsx
+++
@@ -1,9 +0,0 @@
-import { SignInRequestList } from "./sign-in-request-list";
-
-export const SignInRequestContainer = () => {
-  return (
-    <div className="w-full flex flex-col items-center">
-      <SignInRequestList />
-    </div>
-  );
-};
