a36a718ae7e440f592e888e1ffe2a1d6ee1eb9c2
prenaissance <95138356+prenaissance@users.noreply.github.com>
2025-11-15 23:12:45 +0200

    feat(backend): restructure get_otp command

--- src-tauri/src/authentication_approvals/commands.rs
+++ src-tauri/src/authentication_approvals/commands.rs
@@ -1,6 +1,8 @@
+use std::time::{SystemTime, UNIX_EPOCH};
+
 use steamguard::approver::Challenge;
 use steamguard::protobufs::steammessages_auth_steamclient::CAuthentication_GetAuthSessionInfo_Response;
-use steamguard::token::Tokens;
+use steamguard::token::{Tokens, TwoFactorSecret};
 use steamguard::{ApproverError, LoginApprover, SteamGuardAccount};
 
 use super::payloads::{ApproveQrLoginRequest, AuthSessionResponse, GetApprovalsError};
@@ -8,6 +10,27 @@ use crate::AppState;
 use crate::authentication_approvals::payloads::{
     AuthApprovalError, AuthApproveRequest, AuthDenyRequest,
 };
+
+#[tauri::command]
+pub fn get_otp(state: tauri::State<'_, AppState>) -> Option<String> {
+    let shared_secret = state
+        .accounts_config
+        .lock()
+        .unwrap()
+        .get_active_account()
+        .map(|account| account.shared_secret.clone());
+
+    shared_secret.map(|secret| {
+        TwoFactorSecret::parse_shared_secret(secret)
+            .unwrap()
+            .generate_code(
+                SystemTime::now()
+                    .duration_since(UNIX_EPOCH)
+                    .unwrap()
+                    .as_secs(),
+            )
+    })
+}
 
 #[tauri::command]
 pub fn get_sessions(
