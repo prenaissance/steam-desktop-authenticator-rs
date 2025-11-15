5f401fa304f728e013c011f7988f2ee538a1ef90
prenaissance <95138356+prenaissance@users.noreply.github.com>
2025-11-15 22:41:16 +0200

    chore(frontend): use context instead of zustand

--- src/pages/app/confirmations/confirmations.tsx
+++ src/pages/app/confirmations/confirmations.tsx
@@ -3,6 +3,7 @@ import { toast } from "sonner";
 import { useConfirmations } from "~/api/confirmations";
 import { ConfirmationListFooter } from "~/components/confirmations/confirmation-list-footer";
 import { ConfirmationListItem } from "~/components/confirmations/confirmation-list-item";
+import { ConfirmationSelectionProvider } from "~/stores/confirmations/confirmation-selection-store";
 
 export const ConfirmationsPage = () => {
   const { data, error } = useConfirmations();
@@ -15,17 +16,19 @@ export const ConfirmationsPage = () => {
   }, [error]);
 
   return (
-    <div className="flex h-full flex-col">
-      <h1 className="mb-2 text-center font-bold text-2xl">Confirmations</h1>
-      <ul className="flex w-full grow flex-col items-center gap-1 overflow-y-auto p-1">
-        {data?.map((confirmation) => (
-          <ConfirmationListItem
-            key={confirmation.id}
-            confirmation={confirmation}
-          />
-        ))}
-      </ul>
-      <ConfirmationListFooter />
-    </div>
+    <ConfirmationSelectionProvider>
+      <div className="flex h-full flex-col">
+        <h1 className="mb-2 text-center font-bold text-2xl">Confirmations</h1>
+        <ul className="flex w-full grow flex-col items-center gap-1 overflow-y-auto p-1">
+          {data?.map((confirmation) => (
+            <ConfirmationListItem
+              key={confirmation.id}
+              confirmation={confirmation}
+            />
+          ))}
+        </ul>
+        <ConfirmationListFooter />
+      </div>
+    </ConfirmationSelectionProvider>
   );
 };
